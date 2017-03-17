da7e7e741b289ae0498fb6d1adeef70ff3c8449c
Sotheara Leang <leangsotheara@gmail.com>
2017-03-17 22:35:45 +0700

    Commit

--- src/main/java/com/example/springintegration/gateway/ServerTcpGateway.java
+++
@@ -1,8 +0,0 @@
-package com.example.springintegration.gateway;
-
-import org.springframework.util.concurrent.ListenableFuture;
-
-public interface ServerTcpGateway {
-
-	ListenableFuture<Object> send(Object mesage);
-}
