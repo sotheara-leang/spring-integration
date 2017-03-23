fcc27eec45fa57b3264db6937f635404fcff5904
Sotheara Leang <leangsotheara@gmail.com>
2017-03-23 21:56:34 +0700

    Commit

--- src/main/java/com/example/springintegration/server/activator/LogActivator.java
+++ src/main/java/com/example/springintegration/server/activator/LogActivator.java
@@ -8,22 +8,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.integration.history.MessageHistory;
 import org.springframework.messaging.handler.annotation.Header;
 
-import com.solab.iso8583.IsoMessage;
-
 public class LogActivator {
 	
 	private static Logger logger = LoggerFactory.getLogger(LogActivator.class);
 
-	public IsoMessage logHistory(IsoMessage message, @Header(MessageHistory.HEADER_NAME) List<Properties> histories) {
+	public void logHistory(@Header(MessageHistory.HEADER_NAME) List<Properties> histories) {
 		logger.debug("**** Message History ****");
-		logger.debug("message : {}", message.debugString());
 		
 		for (Properties element : histories) {
 			logger.debug(element.toString());
 		}
 		
 		logger.debug("**** Message History ****");
-		
-		return message;
 	}
 }
