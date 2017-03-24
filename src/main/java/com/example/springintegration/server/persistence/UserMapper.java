1b444f453d2fbc0704863140922db9e260b0f048
Sotheara Leang <sotheara.leang@mcnc.co.kr>
2017-03-24 15:22:43 +0700

    Commit

--- src/main/java/com/example/springintegration/server/persistence/UserMapper.java
+++ src/main/java/com/example/springintegration/server/persistence/UserMapper.java
@@ -2,7 +2,12 @@ package com.example.springintegration.server.persistence;
 
 import org.springframework.stereotype.Repository;
 
+import com.example.springintegration.server.domain.User;
+
 @Repository
 public interface UserMapper {
 
+	int insertUser(User user);
+	
+	int deleteUser(User user);
 }
